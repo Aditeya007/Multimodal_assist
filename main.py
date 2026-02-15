398bbdabf9941a2329f4fe7d5868a8b143060b58
Aditeya Mitra <aditeyamitra.kol@gmail.com>
2026-02-15 20:02:05 +0530

    feat: Implement a continuous interaction loop with an exit condition for the chatbot.

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from groq import Groq
 from dotenv import load_dotenv
 load_dotenv()
 bot=Groq(api_key=os.getenv("GROQ_API_KEY"))
-inp=input("How can i help you today , sir?")
-answer=bot.chat.completions.create(
-    model="llama-3.3-70b-versatile",
-    messages=[
-        {"role":"system","content":"You are an intelligent and helpful assistant"},
-        {"role":"user","content":inp}
-    ],
-    temperature=0.4,
-    max_tokens=300
-)
-print("Vision:",answer.choices[0].message.content)
+while True:
+
+    inp=input("How can i help you today , sir?")
+    if inp.lower() in ["exit","bye","sleep","shutdown"]:
+        print("Pleaure working for you")
+        break
+    answer=bot.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {"role":"system","content":"You are an intelligent and helpful assistant"},
+            {"role":"user","content":inp}
+        ],
+        temperature=0.4,
+        max_tokens=300
+    )
+    print("Vision:",answer.choices[0].message.content)
