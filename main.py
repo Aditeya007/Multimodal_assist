7e70a10f0b8b0a59177b67deab822a07edf32ac2
Aditeya Mitra <aditeyamitra.kol@gmail.com>
2026-02-15 19:51:43 +0530

    feat: Introduce a basic Groq API interaction script with environment variable loading and a comprehensive gitignore.

---
+++ main.py
@@ -0,0 +1,16 @@
+import os 
+from groq import Groq
+from dotenv import load_dotenv
+load_dotenv()
+bot=Groq(api_key=os.getenv("GROQ_API_KEY"))
+inp=input("How can i help you today , sir?")
+answer=bot.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[
+        {"role":"system","content":"You are an intelligent and helpful assistant"},
+        {"role":"user","content":inp}
+    ],
+    temperature=0.4,
+    max_tokens=300
+)
+print("Vision:",answer.choices[0].message.content)
